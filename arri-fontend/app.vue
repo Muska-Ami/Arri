37d2bfd3a8eeba2311b7635aef050217abb0423e
夏沫花火zzz <xmcn@1l1.icu>
2023-10-02 20:47:51 +0800

    second commit

--- arri-fontend/app.vue
+++ arri-fontend/app.vue
@@ -1,2 +1,13 @@
 <template>
+  <div :model="context">{{ refresh.message }}</div>
 </template>
+
+<script setup>
+import i18n from "@/script/i18n.js"
+
+const lzf = i18n("zh_CN")
+
+let refresh = ref({
+  message: lzf.noMessage
+})
+</script>
